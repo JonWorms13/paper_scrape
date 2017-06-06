fcec703e6e77f0ea297d0aa6ea514444e8d174b6
JonWorms13 <jonworms@yahoo.com>
2017-06-06 17:51:45 -0400

    Add files via upload

---
+++ paper_scrape/server.js
@@ -0,0 +1,15 @@
+
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var Note = require("./models/Note.js");
+var Article = require("./models/Article.js");
+var request = require("request");
+var cheerio = require("cheerio");
+mongoose.Promise = Promise;
+
+
+var app = express();
+
